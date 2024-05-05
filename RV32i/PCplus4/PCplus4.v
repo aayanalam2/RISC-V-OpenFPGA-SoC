b23b8563e75f7393fac32c01ca545ae0a9b21981
Shmoucloud <127538895+Shmoucloud@users.noreply.github.com>
2024-05-05 14:04:04 +0500

    Update PCplus4.v

--- RV32i/PCplus4/PCplus4.v
+++ RV32i/PCplus4/PCplus4.v
@@ -1,15 +1,1 @@
-module PCplus4(PCin, PCadd);
-	output reg [31:0]PCadd;
-	input [31:0]PCin;
-	
-	initial
-		begin
-		PCadd = 0;
-		end
-	
-	always @(*)
-		begin
-		PCadd = PCin + 4;
-		end
 
-endmodule
